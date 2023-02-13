d738d35549e02ce6771857b3fbbdb000b4775410
mahanteshm94 <mahantesh0917@gmail.com>
2023-02-13 18:30:10 +0530

    msg

---
+++ Givennumber.java
@@ -0,0 +1,29 @@
+class Givennumber
+{
+  static void given(int b)
+  {
+    int sum=0;
+    for(int i=0;i<=b;i++)
+     {
+
+        sum=sum+i;
+             System.out.println(i);
+
+     }
+
+
+      System.out.println(sum);
+
+
+   }
+   public static void main(String[] args)
+
+   {
+   
+   
+   
+        given(25);
+   
+   
+   }
+}
